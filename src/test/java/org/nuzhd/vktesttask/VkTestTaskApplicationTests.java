a7a4c824a47bc25e66cb82095aaa4e225fe4d7cd
yeeeip <dmit.nujdin2014@yandex.ru>
2024-03-13 15:55:00 +0300

    Initial commit

---
+++ src/test/java/org/nuzhd/vktesttask/VkTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nuzhd.vktesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
